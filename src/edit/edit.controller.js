4ea87561890d2652f562b26f2b5412b53696b241
Mark Polak <mark@thedutchies.com>
2015-04-22 18:13:26 +0200

    Add field overrides

--- src/edit/edit.controller.js
+++ src/edit/edit.controller.js
@@ -1,7 +1,59 @@
 class EditController {
-    constructor(modelToEdit, modelName) {
+    constructor(modelToEdit, modelName, d2FormFields) {
         this.modelToEdit = modelToEdit;
         this.modelName = modelName;
+        this.fieldManager = d2FormFields.getManager();
+
+        this.fieldManager
+            .addFieldOverrideFor('type', {
+                type: 'select',
+                options: [
+                    'int',
+                    'string',
+                    'bool',
+                    'trueOnly',
+                    'date',
+                    'username'
+                ],
+                onChange: model => console.log('New type value is ' + model.type)
+            })
+            .addFieldOverrideFor('aggregationOperator', {
+                type: 'select',
+                options: [
+                    'sum',
+                    'average',
+                    'avg',
+                    'count',
+                    'stddev',
+                    'variance',
+                    'min',
+                    'max'
+                ],
+                hideWhen: model => ['bool', 'trueOnly', 'int'].includes(model.type) === false,
+                onChange: model => console.log('New aggregationOperator value is ' + model.aggregationOperator)
+            })
+            .addFieldOverrideFor('numberType', {
+                type: 'select',
+                options: [
+                    'number',
+                    'int',
+                    'posInt',
+                    'negInt',
+                    'zeroPositiveInt',
+                    'unitInterval',
+                    'percentage'
+                ],
+                hideWhen: model => model.type !== 'int',
+                onChange: model => console.log('New numberType value is ' + model.numberType)
+            })
+            .addFieldOverrideFor('textType', {
+                type: 'select',
+                options: [
+                    'text',
+                    'longText'
+                ],
+                hideWhen: model => model.type !== 'string'
+            })
     }
 
     save() {
